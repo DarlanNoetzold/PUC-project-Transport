a5a591e3b4173c27252765722b477e5cfc9b106e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-03-27 08:24:27 -0300

    Exceptions treatment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import time
 
-custo_por_km=0
+#Inicializando variavel global
+custo_por_km = 0
 
 #Leitura dos dados
 dados = pd.read_csv("DNIT-Distancias.csv", sep=";")
@@ -9,6 +11,7 @@ dados = pd.read_csv("DNIT-Distancias.csv", sep=";")
 columns = dados.columns.to_series()
 dados.set_index(columns, inplace=True)
 
+#Inicializa o Menu do Programa e chama as funções de cada opção
 def main():
     load_program = True
     while load_program:
@@ -19,6 +22,8 @@ def main():
         print("4. Terminar o programa")
         print("\nEscolha uma das opcoes: ")
         op = input()
+
+        #Opção de Configurar custo por KM
         if op == "1":
             invalid_op = True
             while invalid_op:
@@ -28,6 +33,8 @@ def main():
                     print("Opcao Invalida, por favor digite um valor maior que 0!")
                 else:
                     invalid_op = False
+
+        #Opção de Consultar Trecho
         elif op == "2":
             if custo_por_km > 0:
                 print("\nDigite a cidade de origem: ")
@@ -36,9 +43,11 @@ def main():
                 print("\nDigite a cidade de destino: ")
                 cidade_destino = input()
                 cidade_destino = cidade_destino.upper()
-                print("Custo de R$ ",consultar_trecho(cidade_origem, cidade_destino, custo_por_km),"\n\n")
+                consultar_trecho(cidade_origem, cidade_destino, custo_por_km)
             else:
                 print("Voce deve informar o custo por KM antes de definir um trecho!")
+
+        #Opção de Consultar Rota
         elif op == "3":
             print("\nDigite o nome de duas ou mais cidades separadas por virgula:")
             string_cidades = input()
@@ -46,29 +55,38 @@ def main():
             lista_cidades = string_cidades.split(", ")
             consultar_rota(lista_cidades, custo_por_km)
 
+        #Opção de Término do Programa
         elif op == "4":
             print("\nFim do programa!")
             load_program = False
         else:
             print("\nOpcao Invalida!")
 
+#Função que Consulta o Trecho que recebe a cidade de origem e de destino e o Custo por KM. E imprime a Distância e o custo
 def consultar_trecho(cidade_origem, cidade_destino, custo_por_km):
-    km = dados.loc[cidade_origem,cidade_destino]
-    print("Distancia entre",cidade_origem,"e", cidade_destino, "e de", km, "km")
-    return km * custo_por_km
+    try:
+        km = dados.loc[cidade_origem,cidade_destino]
+        print("Distancia entre",cidade_origem,"e", cidade_destino, "e de", km, "km")
+        print("Custo de R$", km * custo_por_km,"\n")
+    except:
+        print("Alguma cidade digitada esta incorreta ou não temos em nossos dados!")
 
+#Função que Consulta uma rota que recebe uma lista das cidades e o custo por KM.
 def consultar_rota(lista_cidades, custo_por_km):
-    cont=0
-    total_km= 0
-    while cont < len(lista_cidades) - 1:
-        km = dados.loc[lista_cidades[cont], lista_cidades[cont+1]]
-        total_km = km + total_km
-        print(lista_cidades[cont], "->", lista_cidades[cont+1], "(", km, "KM )")
-        cont = cont + 1
-    print("\nDistância total: ", total_km, "km")
-    print("Custo total: R$ %.2f" %(total_km*custo_por_km))
-    print("O total de litros gastos foi de: %.3f" %(total_km*2.57))
-    print("A viagem durou %d dias.\n" %(total_km/283))
+    try:
+        cont = 0
+        total_km = 0
+        while cont < len(lista_cidades) - 1:
+            km = dados.loc[lista_cidades[cont], lista_cidades[cont+1]]
+            total_km = km + total_km
+            print(lista_cidades[cont], "->", lista_cidades[cont+1], "(", km, "KM )")
+            cont = cont + 1
+        print("\nDistância total: ", total_km, "km")
+        print("Custo total: R$ %.2f" %(total_km*custo_por_km))
+        print("O total de litros gastos foi de: %.3f" %(total_km*2.57))
+        print("A viagem durou %d dias.\n" %(total_km/283))
+    except:
+        print("Alguma cidade digitada esta incorreta ou não temos em nossos dados!")
 
-
+#Chama a função main
 main()
